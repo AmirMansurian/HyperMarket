da7e7847ccf55b6cd8218aa83340db36d081849f
Amir <coloner12@gmail.com>
2020-07-09 13:02:48 -0700

    Persons search designed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -21,22 +21,40 @@ import java.util.*;
 public class Controller {
 
     @FXML
-    private TextField inputtext;
+    private TextField IDText;
 
     @FXML
-    private TextArea outputtext;
+    private TextField FNameText;
 
     @FXML
-    private Label outputlabel;
+    private TextField LNameText;
 
     @FXML
-    private Label intputlabel;
+    private TextField NumberText;
 
     @FXML
-    private Button button;
+    private TextField TypeText;
 
     @FXML
-    private TableView <Person> table;
+    private Label IDLabel;
+
+    @FXML
+    private Label FNameLabel;
+
+    @FXML
+    private Label LNameLabel;
+
+    @FXML
+    private Label NumberLabel;
+
+    @FXML
+    private Label TypeLabel;
+
+    @FXML
+    private Button SearchButton;
+
+    @FXML
+    private TableView <Person> Table;
 
     Connection connection = null;
     PreparedStatement statement = null;
@@ -55,11 +73,58 @@ public class Controller {
     @FXML
     private void handleButtonAction(ActionEvent event) throws SQLException {
 
-        String name = inputtext.getText();
-        String query = "select * from Person.Customer";
+        List <String> Args = new ArrayList<String>();
+        Args.add(IDText.getText());
+        Args.add(FNameText.getText());
+        Args.add(LNameText.getText());
+        Args.add(NumberText.getText());
+        String query = "select * from Person.Customer ";
+
+        int flag = 0;
+        int k=0;
+        for (int i=0; i< Args.size(); i++)
+        {
+            if (!Args.get(i).isEmpty())
+            {
+                if (flag == 0) {
+                    query = query + " Where ";
+                    flag = 1;
+                }
+
+                if (i == 0) {
+                    if (k != 0)
+                        query += " and ";
+                    query += " CustomerId = ? ";
+                }
+                else if (i == 1) {
+                    if (k != 0)
+                        query += " and ";
+                    query += " FirstName = ? ";
+                }
+                else if (i == 2) {
+                    if (k != 0)
+                        query += " and ";
+                    query += " LastName = ? ";
+                }
+                else if (i ==3){
+                    if (k != 0)
+                        query += " and ";
+                    query += " PhoneNumber = ? ";
+                }
+                k++;
+            }
+        }
+
 
         statement = connection.prepareStatement(query);
-        //statement.setString(1, name);
+
+        int j=0;
+        for (int i=0; i< Args.size(); i++)
+        {
+            if (!Args.get(i).isEmpty())
+                statement.setString(++j, Args.get(i));
+        }
+
         resultSet = statement.executeQuery();
 
         ObservableList <Person> data = FXCollections.observableArrayList();
@@ -78,7 +143,7 @@ public class Controller {
             data.add(new Person(list.get(0), list.get(1), list.get(2), list.get(3), list.get(4), list.get(5), list.get(6)));
         }
 
-        System.out.println(data);
+       // System.out.println(data);
 
         TableColumn  ID = new TableColumn("Customer ID");
         ID.setCellValueFactory(new PropertyValueFactory<>("ID"));
@@ -103,9 +168,9 @@ public class Controller {
 
 
 
-        table.setItems(data);
-        table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        table.getColumns().addAll(ID, FName, LName, Gender, BirthDay, PhoneNumber, Dept);
+        Table.setItems(data);
+        Table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        Table.getColumns().addAll(ID, FName, LName, Gender, BirthDay, PhoneNumber, Dept);
 
         }
     }
